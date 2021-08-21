4c9c69408403ac46220d8d9bf27aa27c244353cd
Adam And <andadam24@gmail.com>
2021-08-21 15:51:04 -0400

    deploying

--- models/transaction.js
+++ models/transaction.js
@@ -1,5 +1,15 @@
 const mongoose = require("mongoose");
 
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 const Schema = mongoose.Schema;
 
 const transactionSchema = new Schema(
